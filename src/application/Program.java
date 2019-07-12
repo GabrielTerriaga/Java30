9ba1e1d5ac0ccec32af7c81d2b6dd8611ac68289
Gabriel Terriaga <Gmterriaga0@yahoo.com.br>
2019-07-12 14:14:14 -0300

    Try wih resources

---
+++ src/application/Program.java
@@ -0,0 +1,24 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Program {
+	public static void main(String[] args) {
+		
+		String path = "C:\\temp\\java.txt";
+		
+		//Coloca a instancia dentro do try
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			
+			String line = br.readLine();
+			while (line != null) {
+				System.out.println(line);
+				line = br.readLine();
+			}
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+	}
+}
